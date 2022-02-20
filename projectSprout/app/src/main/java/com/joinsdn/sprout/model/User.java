08172b117951538f44f8ecb06e154b8af126a6e2
Frank Hui <huifrank@live.com>
2022-02-20 04:42:36 -0800

    im the mf json man

--- projectSprout/app/src/main/java/com/joinsdn/sprout/model/User.java
+++ projectSprout/app/src/main/java/com/joinsdn/sprout/model/User.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.Image;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import org.json.*;
 
 import com.joinsdn.sprout.profile.Profile;
 
@@ -21,6 +22,14 @@ public class User {
     private String device_ID;
 
     public User(String firstname, String lastname, String profilePicture, String bio, Context ctx) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.profilePicture = profilePicture;
+        this.bio = bio;
+        this.device_ID = Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    public User(String firstname, String lastname, String profilePicture, String bio) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.profilePicture = profilePicture;
@@ -79,5 +88,34 @@ public class User {
     public void addMatch(User user) {
         if (!this.matches.contains(user))
             this.matches.add(user);
+    }
+
+    public JSONObject toJson() throws JSONException {
+        JSONObject obj = new JSONObject();
+        obj.put("firstname",this.firstname);
+        obj.put("lastname",this.lastname);
+        obj.put("profilepicture",this.profilePicture);
+        obj.put("deviceID",this.device_ID);
+        return obj;
+    }
+
+    public static User fromJson(String input) throws JSONException {
+        JSONObject obj = new JSONObject(input);
+        return fromJson(obj);
+    }
+
+    public static User fromJson(JSONObject obj) throws JSONException {
+        User u = new User(obj.getString("firstname"),obj.getString("lastname"), obj.getString("profilepicture"), obj.getString("deviceId"));
+        return u;
+    }
+
+    @Override
+    public String toString(){
+        try {
+            return toJson().toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "Invalid object";
+        }
     }
 }
