5d77261ec18b3dade9926af34601126b9046ae92
Frank Hui <huifrank@live.com>
2022-02-19 21:05:28 -0800

    Generate user class

--- projectSprout/app/src/main/java/com/joinsdn/sprout/model/User.java
+++ projectSprout/app/src/main/java/com/joinsdn/sprout/model/User.java
@@ -10,17 +10,64 @@ import java.util.List;
 public class User {
 
     private String firstname, lastname;
-    private Image profilePicture;
-    private MediaStore.Images pictures;
+    private String profilePicture;
     private String bio;
 
     private Profile profile;
     private List<User> matches;
 
-    // PRE:
-    // POST:
-    public User() {
-
+    public User(String firstname, String lastname, String profilePicture, String bio) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.profilePicture = profilePicture;
+        this.bio = bio;
     }
 
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public List<User> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<User> matches) {
+        this.matches = matches;
+    }
 }
